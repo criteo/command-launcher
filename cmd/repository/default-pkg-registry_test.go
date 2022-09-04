9ec74046aca2f07d1ba41c3c20a74dacc8c1f7d9
bhou-crto <b.hou@criteo.com>
2022-09-04 19:36:21 +0200

    Enable benchmark in tests

--- cmd/repository/default-pkg-registry_test.go
+++ cmd/repository/default-pkg-registry_test.go
@@ -128,5 +128,7 @@ func BenchmarkLoadLargeRegistry(t *testing.B) {
 	loadedExeCmds := loadedReg.ExecutableCommands()
 	assert.Equal(t, len(exeCmds), len(loadedExeCmds), "should have same number of executable cmd")
 
-	//assert.Fail(t, "")
+	// assert.Fail(t, "")
+	// registry load time must less than 0.1 second (100 ms)
+	assert.True(t, elapsed.Seconds() < 0.1)
 }
