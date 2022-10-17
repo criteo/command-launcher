bb73746bd9abe93d6a2fc81f5c96ba411c5d6750
Stephane Binetruy <s.binetruy@criteo.com>
2022-10-17 16:27:41 +0200

    [Package command] Add install command
    - Install a pkg from an url or a pathname
    - Install git repo from the git url

--- cmd/package.go
+++ cmd/package.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/criteo/command-launcher/cmd/remote"
@@ -10,6 +14,7 @@ import (
 	"github.com/criteo/command-launcher/internal/config"
 	"github.com/criteo/command-launcher/internal/console"
 	"github.com/criteo/command-launcher/internal/context"
+	"github.com/criteo/command-launcher/internal/helper"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -79,6 +84,13 @@ func AddPackageCmd(rootCmd *cobra.Command, appCtx context.LauncherContext) {
 		Example: fmt.Sprintf(`
   %s package install --git https://example.com/my-repo.git my-pkg`, appCtx.AppName()),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if packageFlags.fileUrl != "" {
+				return installZipFile(packageFlags.fileUrl)
+			}
+
+			if packageFlags.gitUrl != "" {
+				return installGitRepo(packageFlags.gitUrl)
+			}
 
 			return nil
 		},
@@ -162,4 +174,72 @@ func printCommands(commands []command.Command) {
 			}
 		}
 	}
+}
+
+func installGitRepo(gitUrl string) error {
+	_, err := url.Parse(gitUrl)
+	if err != nil {
+		return fmt.Errorf("invalid url or pathname: %v", err)
+	}
+
+	ctx := exec.Command("git", "clone", gitUrl)
+	ctx.Dir = viper.GetString(config.DROPIN_FOLDER_KEY)
+	ctx.Stdout = os.Stdout
+	ctx.Stderr = os.Stderr
+	ctx.Stdin = os.Stdin
+
+	if err = ctx.Run(); err != nil {
+		return fmt.Errorf("git command has failed: %v", err)
+	}
+
+	repoDir := strings.ReplaceAll(filepath.Base(gitUrl), filepath.Ext(gitUrl), "")
+	path := filepath.Join(viper.GetString(config.DROPIN_FOLDER_KEY), repoDir)
+	pkg, err := remote.CreateFolderPackage(path)
+	if err != nil {
+		os.RemoveAll(path)
+		return fmt.Errorf("the git repo does not contain a manifest file: %v", err)
+	}
+
+	console.Success("Package %s installed in the dropin repository", pkg.Name())
+	return nil
+}
+
+func installZipFile(fileUrl string) error {
+	url, err := url.Parse(fileUrl)
+	if err != nil {
+		return fmt.Errorf("invalid url or pathname: %v", err)
+	}
+
+	var pathname string
+	if url.IsAbs() {
+		if url.Scheme == "file" {
+			pathname = url.Path
+		} else {
+			tmpDir, err := os.MkdirTemp("", "package-download-*")
+			if err != nil {
+				return fmt.Errorf("cannot create temporary dir (%v)", err)
+			}
+			defer os.RemoveAll(tmpDir)
+
+			pkgPathname := filepath.Join(tmpDir, filepath.Base(url.Path))
+			if err := helper.DownloadFile(fileUrl, pkgPathname, true); err != nil {
+				return fmt.Errorf("error downloading %s: %v", url, err)
+			}
+
+		}
+	} else {
+		pathname = url.Path
+	}
+
+	pkg, err := remote.CreateZipPackage(pathname)
+	if err != nil {
+		return fmt.Errorf("cannot create the package from the zip file: %v", err)
+	}
+
+	mf, err := pkg.InstallTo(viper.GetString(config.DROPIN_FOLDER_KEY))
+	if err == nil {
+		console.Success("Package %s installed in the dropin repository", mf.Name())
+	}
+
+	return err
 }
