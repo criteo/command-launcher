8600123c985856ac43e340ea06ffa7d5cbe4e68f
bhou-crto <b.hou@criteo.com>
2022-11-01 09:40:06 +0100

    Fix integration test

--- test/packages-src/flag-env/bonjour.sh
+++ test/packages-src/flag-env/bonjour.sh
@@ -5,3 +5,7 @@ echo $CL_LOG_LEVEL
 echo $CL_FLAG_NAME
 echo $CL_FLAG_LANGUAGE
 echo $CL_ARG_1
+
+echo "cola flag: $COLA_FLAG_NAME"
+echo "cola flag: $COLA_FLAG_LANGUAGE"
+echo "cola arg: $COLA_ARG_1"
