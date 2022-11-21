3f7207eacf43c795fe444b76c3924e7e77c81552
alien: Hedi Ghediri <h.ghediri@criteo.com>
2022-11-21 22:16:20 +0100

    Remove superfluous 'from' from login help
    And add colon to separate descriptor / value in options 1 & 2.

--- cmd/login.go
+++ cmd/login.go
@@ -36,10 +36,10 @@ func AddLoginCmd(rootCmd *cobra.Command, appCtx context.LauncherContext) {
 		Long: fmt.Sprintf(`
 Login to use services.
 
-You can specify the your password from:
-1. command option --password (-p)
-2. environment variable %s
-3. from command line input
+You can specify your password from:
+1. command option: --password (-p)
+2. environment variable: %s
+3. command line input
 
 The credential will be stored in your system vault.`, appCtx.PasswordEnvVar()),
 		RunE: func(cmd *cobra.Command, args []string) error {
