de0603ede4e170b5a0d82d5170ee9dce931b4843
Jacobo de Vera <devel@jacobodevera.com>
2024-08-24 12:33:25 +0100

    Add missing variable on Windows tests

--- test/packages-src/bonjour/bonjour.bat
+++ test/packages-src/bonjour/bonjour.bat
@@ -4,6 +4,7 @@ ECHO %CL_FLAG_LANGUAGE%
 ECHO %CL_ARG_1%
 
 ECHO packge dir: %CL_PACKAGE_DIR%
+ECHO command name: %CL_COMMAND_NAME%
 
 ECHO cola flag: %COLA_FLAG_NAME%
 ECHO cola flag: %COLA_FLAG_LANGUAGE%
